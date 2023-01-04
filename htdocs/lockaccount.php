3b504bb981083536185c9b2f076cba56de15e235
philha <philha@worteks.com>
2023-01-04 10:51:53 +0100

    service-desk usage of ltb/ldap composer package
    - copied from white-pages
    - new $ldap_network_timeout global variable
    - match Ltb\Ldap::connect( ) signature with network timeout

--- htdocs/lockaccount.php
+++ htdocs/lockaccount.php
@@ -16,10 +16,10 @@ if (isset($_POST["dn"]) and $_POST["dn"]) {
 if ($result === "") {
 
     require_once("../conf/config.inc.php");
-    require_once("../lib/ldap.inc.php");
+    require __DIR__ . '/vendor/autoload.php';
 
     # Connect to LDAP
-    $ldap_connection = wp_ldap_connect($ldap_url, $ldap_starttls, $ldap_binddn, $ldap_bindpw);
+    $ldap_connection = \Ltb\Ldap::connect($ldap_url, $ldap_starttls, $ldap_binddn, $ldap_bindpw, $ldap_network_timeout);
 
     $ldap = $ldap_connection[0];
     $result = $ldap_connection[1];
--- htdocs/searchlocked.php
+++ htdocs/searchlocked.php
@@ -11,11 +11,11 @@ $size_limit_reached = false;
 if ($result === "") {
 
     require_once("../conf/config.inc.php");
-    require_once("../lib/ldap.inc.php");
+    require __DIR__ . '/vendor/autoload.php';
     require_once("../lib/date.inc.php");
 
     # Connect to LDAP
-    $ldap_connection = wp_ldap_connect($ldap_url, $ldap_starttls, $ldap_binddn, $ldap_bindpw);
+    $ldap_connection = \Ltb\Ldap::connect($ldap_url, $ldap_starttls, $ldap_binddn, $ldap_bindpw, $ldap_network_timeout);
 
     $ldap = $ldap_connection[0];
     $result = $ldap_connection[1];
