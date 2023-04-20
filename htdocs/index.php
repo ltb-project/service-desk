a3c453bb1e708da7d2b3f7bd44c9f6e0036244a7
Clément OUDOT <clement@oodo.net>
2023-04-20 09:15:40 +0200

    Create displayer for address

--- htdocs/index.php
+++ htdocs/index.php
@@ -88,6 +88,7 @@ require_once("../lib/smarty.inc.php");
 $smarty->registerPlugin("function", "get_attribute", "get_attribute");
 $smarty->registerPlugin("function", "convert_ldap_date", "convert_ldap_date");
 $smarty->registerPlugin("function", "convert_bytes", "convert_bytes");
+$smarty->registerPlugin("function", "split_value", "split_value");
 
 #==============================================================================
 # Route to page
