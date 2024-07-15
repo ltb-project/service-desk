30c40efd859e0af162184550648180f9ba69a42e
Clément OUDOT <clement@oodo.net>
2024-07-15 19:12:50 +0200

    Function to convert AD date

--- lib/smarty.inc.php
+++ lib/smarty.inc.php
@@ -64,6 +64,11 @@ function convert_ldap_date($date) {
 
 }
 
+function convert_ad_date($date) {
+
+    return adDate2phpDate( $date );
+
+}
 function convert_bytes($bytes) {
 
     return FileSizeConvert( $bytes );
