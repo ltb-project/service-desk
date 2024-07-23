9d8f9a7f1432dc852d540d2f50ec9f2f079eff8b
Clément OUDOT <clement.oudot@worteks.com>
2024-07-23 10:12:35 +0200

    Improve code for comment in audit

--- lib/audit.inc.php
+++ lib/audit.inc.php
@@ -1,26 +1,19 @@
 <?php
 function auditlog($file, $dn, $admin, $action, $result, $comment) {
-  if (empty($comment)) {
-    $log = array (
-      "date" => date_format(date_create(), "D, d M Y H:i:s"),
-      "ip" => $_SERVER['REMOTE_ADDR'],
-      "user_dn" => $dn,
-      "done_by" => $admin,
-      "action" => $action,
-      "result" => $result
-    );  
+
+  $log = array(
+    "date" => date_format(date_create(), "D, d M Y H:i:s"),
+    "ip" => $_SERVER['REMOTE_ADDR'],
+    "user_dn" => $dn,
+    "done_by" => $admin,
+    "action" => $action,
+    "result" => $result
+  );
+
+  if ($comment) {
+    $log['comment'] = $comment;
   }
-  else {
-    $log = array (
-      "date" => date_format(date_create(), "D, d M Y H:i:s"),
-      "ip" => $_SERVER['REMOTE_ADDR'],
-      "user_dn" => $dn,
-      "done_by" => $admin,
-      "action" => $action,
-      "result" => $result,
-      "comment" => $comment
-    );
-  }
+
   file_put_contents($file, json_encode($log, JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES) . PHP_EOL, FILE_APPEND | LOCK_EX);
 }
 ?>
