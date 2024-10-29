e5c1dac402e025a51fa561bb4e62156e49a99a1d
Clément OUDOT <clement@oodo.net>
2024-10-29 18:39:17 +0100

    Hook command is specific for password reset

--- lib/hook.inc.php
+++ lib/hook.inc.php
@@ -2,16 +2,16 @@
 
 # Code taken from LTB Self Service Password
 
-/* @function string hook_command(string $hook, string  $login, string $newpassword, null|string $oldpassword, null|boolean $hook_password_encodebase64)
-   Creates the command line to execute for the prehook/posthook process. Passwords will be base64 encoded if configured. Base64 encoding will prevent passwords with special
+/* @function string password_hook_command(string $hook, string  $login, string $newpassword, null|string $oldpassword, null|boolean $hook_password_encodebase64)
+   Creates the command line to execute for the prehook/posthook for password reste. Passwords will be base64 encoded if configured. Base64 encoding will prevent passwords with special
    characters to be modified by the escapeshellarg() function.
    @param $hook string script/command to execute for procesing hook data
    @param $login string username to change/set password for
    @param $newpassword string new passwword for given login
    @param $oldpassword string old password for given login
    @param hook_password_encodebase64 boolean set to true if passwords are to be converted to base64 encoded strings
-*/
-function hook_command($hook, $login, $newpassword, $oldpassword = null, $hook_password_encodebase64 = false) {
+ */
+function password_hook_command($hook, $login, $newpassword, $oldpassword = null, $hook_password_encodebase64 = false) {
 
     $command = '';
     if ( isset($hook_password_encodebase64) && $hook_password_encodebase64 ) {
@@ -28,6 +28,16 @@ function hook_command($hook, $login, $newpassword, $oldpassword = null, $hook_pa
             $command .= ' '.escapeshellarg($oldpassword);
         }
     }
+    return $command;
+}
+
+/* @function string hook_command(string $hook, string $login)
+   Creates hook command line passing login as parameter
+   @param $hook string script/command to execute for procesing hook data
+   @param $login string username
+ */
+function hook_command($hook, $login) {
+    $command = escapeshellcmd($hook).' '.escapeshellarg($login);
     return $command;
 }
 
