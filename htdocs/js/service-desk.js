b2f06f5dbebececcf149a48a9524cf703f99731e
Clément OUDOT <clement@oodo.net>
2025-03-14 16:27:04 +0100

    Manager several dn_link items in a form

--- htdocs/js/service-desk.js
+++ htdocs/js/service-desk.js
@@ -6,33 +6,32 @@ $(document).ready(function(){
     $(this).css("-webkit-text-security", "circle");
   });
 
-  $(".dn_link input").on("keyup", function (event) {
+  $(".dn_link_container input[type=text]").on("keyup", function (event) {
     // Remove value if field is emptied
-    if ($(".dn_link input[type=text]").val().length == 0) {
-        $('.dn_link input[type=hidden]').val('') ;
+    if ($(this).val().length == 0) {
+        $(this).siblings('input[type=hidden]').val('') ;
     }
     // Minimal search characters
-    if ($(".dn_link input[type=text]").val().length > 2) {
+    if ($(this).val().length > 2) {
       if (timer) {
         clearTimeout(timer);
         // clear any existing list
-        $("div.dn_link_suggestions").empty();
+        $(this).siblings('div.dn_link_suggestions').empty();
       }
 
       timer = setTimeout(() => {
-        $.post("index.php", { 'apiendpoint': 'search_dn', 'search': $(".dn_link input[type=text]").val() }, (data) => {
+        $.post("index.php", { 'apiendpoint': 'search_dn', 'search': $(this).val() }, (data) => {
           // clear existing list
-          $('div.dn_link_suggestions').empty();
+          $(this).siblings('div.dn_link_suggestions').empty();
           // add entries to list
           data.forEach( (entry) => {
-console.log(entry);
             const $elem = $(`<button type="button" class="list-group-item list-group-item-action">${entry.display}</button>`);
             $elem.on('click', () => {
-              $('.dn_link input[type=text]').val(entry.display);
-              $('.dn_link input[type=hidden]').val(entry.dn);
-              $('div.dn_link_suggestions').empty();
+              $(this).val(entry.display);
+              $(this).siblings('input[type=hidden]').val(entry.dn);
+              $(this).siblings('div.dn_link_suggestions').empty();
             })
-            $('div.dn_link_suggestions').append($elem)
+            $(this).siblings('div.dn_link_suggestions').append($elem);
           });
         }, 'json');
       }, 500);
