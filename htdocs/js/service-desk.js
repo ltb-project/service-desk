da5c7ed515909abb591692270bbf3aef03fd4857
Clément OUDOT <clement@oodo.net>
2025-03-20 09:43:52 +0100

    Manage mutlivalued dn_link attributes

--- htdocs/js/service-desk.js
+++ htdocs/js/service-desk.js
@@ -46,12 +46,14 @@ $(document).ready(function(){
       var item = $(this).attr('data-item');
       var length = $(this).closest('.value_editor_container').siblings('.value_editor_container').length;
       var newindex = length + 1;
-      var clone = $(this).closest('.value_editor_container').clone();
-      clone.find('.value_editor_field input:first').val('');
-      clone.find('.value_editor_field input:first').attr('name', item + '' + newindex);
+      var clone = $(this).closest('.value_editor_container').clone(true);
+      clone.find('.value_editor_field input[data-role=display]').val('');
+      clone.find('.value_editor_field input[data-role=value]').val('');
+      clone.find('.value_editor_field input[data-role=value]').attr('name', item + '' + newindex);
       clone.find('.value_editor_button button').removeClass('btn-success').addClass('btn-danger');
       clone.find('.value_editor_button button').attr('data-action','del');
       clone.find('.value_editor_button button').attr('data-index', newindex);
+      clone.find('.value_editor_button button').off("click");
       clone.find('.value_editor_button button').on("click", delValueEditor);
       clone.find('.value_editor_button button span').removeClass('fa-plus').addClass('fa-minus');
       $(this).closest('.value_editor_container').parent().append(clone);
