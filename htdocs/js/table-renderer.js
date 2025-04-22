84db958add8fb118a5288ffd039bc654ba905107
David Coutadeur <david.coutadeur@worteks.com>
2025-04-22 18:48:29 +0200

    add js rendering functions (#211)

--- htdocs/js/table-renderer.js
+++ htdocs/js/table-renderer.js
@@ -1,4 +1,167 @@
-function ldapTypeRenderer(column, type, data, dn)
+/* ldapTypeRenderer:
+     column: column name, ie key of $attributes_map associative array (firstname, fullname, identifier,...)
+     type: attribute type, ie value of "type" key in $attributes_map associative array (text, tel, dn_link,...)
+     data: attribute value (javascript array or string)
+     dn: DN of current attribute
+     messages: associative array containing all messages for selected language
+     listing_linkto: array or string containing the attribute key(s) for linking
+     show_undef: boolean. When true, show a specific message when there is no value for the current attribute
+     truncate_value_after: integer. max length after which the string is truncated
+     search: string, parameter named "search" of the http query
+*/
+function ldapTypeRenderer(column, type, data, dn, messages, listing_linkto, show_undef, truncate_value_after, search)
 {
-    return "dn = " + dn + " column = " + column + " type = " + type + " data = " + data;
+    var render = "";
+
+    // if we are processing column "linkto"
+    if(Array.isArray(listing_linkto) && listing_linkto.includes(column))
+    {
+        // TODO: url escape of dn
+        render += "<a href=\"index.php?page=display&dn=" + dn + "&search=" + search + "\" title=\"" + messages["displayentry"] + "\">";
+    }
+
+    // empty value, return immediately the value
+    if(!data || (Array.isArray(data) && !data.length) )
+    {
+        if(show_undef)
+        {
+            render += "<i>" + messages["notdefined"] + "</i>";
+        }
+        else
+        {
+            render += "&nbsp;";
+        }
+    }
+    // value not empty
+    else
+    {
+        // force data to be an array, for treating it as multivalue
+        if(!Array.isArray(data))
+        {
+            data = [data];
+        }
+
+        data.forEach((value) =>
+        {
+            switch(type)
+            {
+                case "text":
+                    render += ldapTextTypeRenderer(column, type, value, dn, messages, listing_linkto, show_undef, truncate_value_after, search);
+                    break;
+                case "mailto":
+                    render += ldapMailtoTypeRenderer(column, type, value, dn, messages, listing_linkto, show_undef, truncate_value_after, search);
+                    break;
+                case "tel":
+                    render += ldapTelTypeRenderer(column, type, value, dn, messages, listing_linkto, show_undef, truncate_value_after, search);
+                    break;
+                case "boolean":
+                    render += ldapBooleanTypeRenderer(column, type, value, dn, messages, listing_linkto, show_undef, truncate_value_after, search);
+                    break;
+                case "date":
+                    render += ldapDateTypeRenderer(column, type, value, dn, messages, listing_linkto, show_undef, truncate_value_after, search);
+                    break;
+                case "ad_date":
+                    render += ldapADDateTypeRenderer(column, type, value, dn, messages, listing_linkto, show_undef, truncate_value_after, search);
+                    break;
+                case "static_list":
+                    render += ldapListTypeRenderer(column, type, value, dn, messages, listing_linkto, show_undef, truncate_value_after, search);
+                    break;
+                case "list":
+                    render += ldapListTypeRenderer(column, type, value, dn, messages, listing_linkto, show_undef, truncate_value_after, search);
+                    break;
+                case "bytes":
+                    render += ldapBytesTypeRenderer(column, type, value, dn, messages, listing_linkto, show_undef, truncate_value_after, search);
+                    break;
+                case "timestamp":
+                    render += ldapTimestampTypeRenderer(column, type, value, dn, messages, listing_linkto, show_undef, truncate_value_after, search);
+                    break;
+                case "dn_link":
+                    render += ldapDNLinkTypeRenderer(column, type, value, dn, messages, listing_linkto, show_undef, truncate_value_after, search);
+                    break;
+                case "ppolicy_dn":
+                    render += ldapPPolicyDNTypeRenderer(column, type, value, dn, messages, listing_linkto, show_undef, truncate_value_after, search);
+                    break;
+                case "address":
+                    render += ldapAddressTypeRenderer(column, type, value, dn, messages, listing_linkto, show_undef, truncate_value_after, search);
+                    break;
+            }
+        });
+    }
+
+    // if we are processing column "linkto"
+    if(Array.isArray(listing_linkto) && listing_linkto.includes(column))
+    {
+        render += "</a>";
+    }
+
+
+    return render;
 }
+
+function ldapTextTypeRenderer(column, type, value, dn, messages, listing_linkto, show_undef, truncate_value_after, search)
+{
+    text = truncate(value, truncate_value_after) + "<br />";
+    return text;
+}
+
+function ldapMailtoTypeRenderer(column, type, value, dn, messages, listing_linkto, show_undef, truncate_value_after, search)
+{
+    return value;
+}
+
+function ldapTelTypeRenderer(column, type, value, dn, messages, listing_linkto, show_undef, truncate_value_after, search)
+{
+    return value;
+}
+
+function ldapBooleanTypeRenderer(column, type, value, dn, messages, listing_linkto, show_undef, truncate_value_after, search)
+{
+    return value;
+}
+
+function ldapADDateTypeRenderer(column, type, value, dn, messages, listing_linkto, show_undef, truncate_value_after, search)
+{
+    return value;
+}
+
+function ldapListTypeRenderer(column, type, value, dn, messages, listing_linkto, show_undef, truncate_value_after, search)
+{
+    return value;
+}
+
+function ldapBytesTypeRenderer(column, type, value, dn, messages, listing_linkto, show_undef, truncate_value_after, search)
+{
+    return value;
+}
+
+function ldapTimestampTypeRenderer(column, type, value, dn, messages, listing_linkto, show_undef, truncate_value_after, search)
+{
+    return value;
+}
+
+function ldapDNLinkTypeRenderer(column, type, value, dn, messages, listing_linkto, show_undef, truncate_value_after, search)
+{
+    return value;
+}
+
+function ldapPPolicyDNTypeRenderer(column, type, value, dn, messages, listing_linkto, show_undef, truncate_value_after, search)
+{
+    return value;
+}
+
+function ldapAddressTypeRenderer(column, type, value, dn, messages, listing_linkto, show_undef, truncate_value_after, search)
+{
+    return value;
+}
+
+function truncate(string, length)
+{
+    result = string;
+    if(length && string.length > length)
+    {
+        result = string.substring(0, length) + "...";
+    }
+
+    return result;
+}
+
