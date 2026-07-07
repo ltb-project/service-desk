1f27f0f14f9f03693953241460216c0fe01b5ac4
Clément OUDOT <clement.oudot@worteks.com>
2026-07-07 17:34:16 +0200

    Add hook for updateGroupMembership

--- htdocs/api/update_group_membership.php
+++ htdocs/api/update_group_membership.php
@@ -5,8 +5,6 @@ $userdn = $_POST["userdn"];
 $dn = $_POST["dn"];
 $checked = $_POST["checked"];
 
-error_log("USER DN ".$userdn." - DN ".$dn." CHECKED ".$checked);
-
 if (!$use_groupmembership) {
     $data['error'] = "featurenoteanbled";
 } else {
@@ -19,6 +17,8 @@ if (!$use_groupmembership) {
 
     if ($ldap) {
 
+        require_once("../lib/hook.inc.php");
+
         # User DN and Group DN match
         $user_dn_match = $ldapInstance->matchDn($userdn, $dnAttribute, $ldap_user_filter, $ldap_user_base, $ldap_scope);
         $group_dn_match = $ldapInstance->matchDn($dn, $dnAttribute, $ldap_group_filter, $ldap_group_base, $ldap_scope);
@@ -30,22 +30,39 @@ if (!$use_groupmembership) {
             $group_modify = false;
             $audit_action = "";
             $result = "";
-            if ($checked == "true") {
-                $group_modify = ldap_mod_add($ldap, $dn, array( $ldap_group_member_attribute => $userdn ));
-                $audit_action = "addusertogroup";
-            } else if ($checked == "false") {
-                $group_modify = ldap_mod_del($ldap, $dn, array( $ldap_group_member_attribute => $userdn ));
-                $audit_action = "removeuserfromgroup";
+            $infos = array();
+
+            list($prehook_return, $prehook_message, $userdn, $infos) =
+                hook($hook_config['updateGroupMembership']['before'] ?? null, 'updateGroupMembership', "", array("dn" => $userdn, "group_dn" => $dn, "checked" => $checked));
+
+            if ( $prehook_return > 0 and !$hook['updateGroupMembership']['before']['ignoreError']) {
+                $data["error"] = "hookerror";
+                $result = "hookerror";
+                error_log("Prehook failed for group membership update (group $dn and user $userdn)");
+            } else {
+                if ($checked == "true") {
+                    $group_modify = ldap_mod_add($ldap, $dn, array( $ldap_group_member_attribute => $userdn ));
+                    $audit_action = "addusertogroup";
+                } else if ($checked == "false") {
+                    $group_modify = ldap_mod_del($ldap, $dn, array( $ldap_group_member_attribute => $userdn ));
+                    $audit_action = "removeuserfromgroup";
+                }
+
+                if (!$group_modify) {
+                    $data["error"] = "groupupdateerror";
+                    $result = "groupupdateerror";
+                    error_log("Modification of group $dn for user $userdn failed");
+                } else {
+                    $result = "groupupdatesuccess";
+                    error_log("Modification of group $dn for user $userdn succeeded");
+                }
             }
 
-            if (!$group_modify) {
-                $data["error"] = "groupupdateerror";
-                $result = "groupupdateerror";
-                error_log("Modification of group $dn for user $userdn failed");
-            } else {
-                $result = "groupupdatesuccess";
-                error_log("Modification of group $dn for user $userdn succeeded");
+            if ( $result === "groupupdatesuccess" ) {
+                list($posthook_return, $posthook_message) =
+                    hook($hook_config['updateGroupMembership']['after'] ?? null, 'updateGroupMembership', "", array("dn" => $userdn, "group_dn" => $dn, "checked" => $checked));
             }
+
             if ($audit_log_file) {
                 auditlog($audit_log_file, $userdn, $audit_admin, $audit_action, $result, $comment);
             }
